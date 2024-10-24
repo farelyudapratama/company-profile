4fd93484bfdc9c2c7e0a4f9daa232e95f410a59e
farelyudapratama <dhakayudha2@gmail.com>
2024-10-24 11:16:31 +0700

    first commit 2

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,8 +17,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="bg-gray-100 font-poppins">
